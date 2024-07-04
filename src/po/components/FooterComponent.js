9cec9abd08a4c2788061861fcf6f0e027a7e8522
Maksym Khomenko <Maksym_Khomenko@epam.com>
2024-07-04 11:56:58 +0300

    fifth_commit

--- src/po/components/FooterComponent.js
+++ src/po/components/FooterComponent.js
@@ -5,26 +5,26 @@ export class FooterComponent {
   }
 
   async selectFooterOption(parameter) {
-    const selectors = {
+    const options = {
       "Sign in/Register": "Sign in / Register",
       "Sell an item": "Sell an item",
-      "Home": "Home",
+      Home: "Home",
       "Help Contact": "Help & Contact",
       "Download the free eBay app": "Download the free eBay app",
     };
-    const selectorName = selectors[parameter];
-    if (selectorName) {
+    const optionName = options[parameter];
+    if (optionName) {
       const elements = await this.#page.$$(
         "li.gh-group__item a.gh-group__link",
       );
       for (const element of elements) {
         const textContent = await element.textContent();
-        if (textContent && textContent.trim() === selectorName) {
+        if (textContent && textContent.trim() === optionName) {
           await element.scrollIntoViewIfNeeded();
           return element;
         }
       }
-      throw new Error(`Element with text '${selectorName}' not found`);
+      throw new Error(`Element with text '${optionName}' not found`);
     } else {
       throw new Error(`Selector for parameter ${parameter} not found`);
     }
