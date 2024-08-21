c0d2013483c8667f3a241e3ba6eb8887ee3073ca
Maksym Khomenko <Maksym_Khomenko@epam.com>
2024-08-21 12:51:37 +0300

    refactoring_fourth_commit

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,12 @@ export default [
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "warn",
+      "no-eval": "warn",
+      "no-redeclare": "warn",
+      "no-unused-expressions": "warn",
+      "no-implied-eval": "warn",
+      "no-iterator": "warn",
+      "no-label-var": "warn",
     },
   },
 ];
